0ec472061ec31659875cb8c71abc6914a5b8edb2
Arva Lazuardy Tristrian 25 <lazuarva@gmail.com>
2025-10-25 21:03:04 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,69 @@
-import React from 'react'
 
-const App = () => {
+function App() {
+  const task = [
+    {
+    title: 'fafafafa',
+    description: 'aaa',
+    date: 'date',
+    time: 'staet',
+    status: 'alllll',
+    },
+
+    {
+    title: 'ufuufeee',
+    description: 'aadadada',
+    date: 'adwadada',
+    time: 'satatas',
+    status: 'adadada',
+    },
+    
+    {
+    title: 'hehe',
+    description: 'aa',
+    date: 'adwadada',
+    time: 'satatas',
+    status: 'adadada',
+    },
+  ]
+
+
+/*  
+  const divArray = [];
+  
+  for (let i = 0; i < task.length; i++ ) 
+    {
+    let currentTask = task[i]
+    divArray.push(
+      <div className='bg-gray-700 p-4 rounded-2xl shadow-2xl'>
+        <span className='font-semibold text-2xl '>
+        {currentTask.title}
+        </span>
+        <br/>
+        <br/>
+        <span className='text-base'>
+          {currentTask.description} 
+          <br/>
+          {currentTask.date} 
+          <br/>
+          {currentTask.time} 
+          <br/>
+          {currentTask.status} 
+          <br/>
+        </span>
+      </div>
+    )    
+  }
+  
+nanti pake {divarray}
+*/
+
   return (
     <div className='flex justify-center w-full min-h-screen bg-gray-800 text-white'>
       <header className='absolute top-0 text-xl p-5 bg-gray-600 w-full text-center rounded-lg'>
         To-Do List App
       </header>  
-        <main className='pt-36 w-3/4'>
+      <main className='pt-36 w-3/4'>
+        
         {/* User Prompt*/}
         <div className='flex justify-center'>
           <input className='bg-slate-700 p-4 rounded-2xl w-3/4 shadow-md' placeholder='Type Your Input'>
@@ -22,40 +79,35 @@ const App = () => {
         {/* To Do List*/}
         <div className='flex justify-center'>
           <div className='w-[85%] flex flex-col gap-y-4'>
-            <p className='font-semibold text-2xl'> Your-To-Do-List</p>
+            <p className='font-semibold text-2xl'> 
+              Your-To-Do-List
+            </p>
             <hr/>
-
-            {/* Card 1*/}
-            <div className='bg-gray-700 p-4 rounded-2xl shadow-2xl'>
-              <span className='font-semibold text-2xl '>
-              Type  
-              </span>
-              <br/>
-              <br/>
-              <span className='text-base'>
-                Why did the chicken <br/>
-                Date <br/>
-                Blabla <br/>
+            
+            {/*Assignment List*/}
+            {task.map((currentTask, index) => (
+              <div className='bg-gray-700 p-4 rounded-2xl shadow-lg' key={index}>
+                <span className='font-semibold text-2xl '>
+                {currentTask.title}
                 </span>
-            </div>
-
-
-            {/* Card 1*/}
-            <div className='bg-gray-700 p-4 rounded-2xl shadow-2xl'>
-              <span className='font-semibold text-2xl '>
-              Shit 
-              </span>
-              <br/>
-              <br/>
-              <span className='text-base'>
-                Why did the chicken <br/>
-                Date <br/>
-                Blabla <br/>
+                <br/>
+                <br/>
+                <span className='text-base'>
+                  {currentTask.description} 
+                  <br/>
+                  {currentTask.date} 
+                  <br/>
+                  {currentTask.time} 
+                  <br/>
+                  {currentTask.status} 
+                  <br/>
                 </span>
-            </div>
+              </div>
+            ) )} 
+            
           </div>
         </div>
-        </main>
+      </main>
     </div>
   )
 }
